f018e544bfee9c78eb19e55d039e2226681fa7f8
Aiyasha99 <aiyashapinjari99@gmail.com>
2024-06-24 13:27:08 +0530

    create listing api route

---
+++ api/controllers/listing.controller.js
@@ -0,0 +1,11 @@
+import Listing from '../models/listing.model.js';
+import { errorHandler } from '../utils/error.js';
+
+export const createListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.create(req.body);
+    return res.status(201).json(listing);  // something crated =status(201)
+  } catch (error) {
+    next(error);
+  }
+};
