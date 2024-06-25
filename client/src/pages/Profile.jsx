08c995099a0ddbdeefd58d310d983d5dfd09885b
Aiyasha99 <aiyashapinjari99@gmail.com>
2024-06-25 22:46:17 +0530

    delete listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -117,6 +117,24 @@ const dispatch = useDispatch();
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   console.log( formData);
     return (
