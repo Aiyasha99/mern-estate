f515dea66367cb8452b3a1f94c08297a88bf976c
Aiyasha99 <aiyashapinjari99@gmail.com>
2024-06-25 22:21:50 +0530

    COMPLETE SHOW USER LISTING FUNCTIONALITY

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import bcryptjs from 'bcryptjs';
 
 import User from '../models/user.model.js';
+
+import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
 
 export const  test = (req,res)=>{
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -34,8 +34,9 @@ export default function Profile() {
   const { currentUser, error} = useSelector((state) => state.user);
   const[file, setFile]= useState(undefined)
   const [formData, setFormData] = useState({});
-  
+  const [showListingsError, setShowListingsError] = useState(false);
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [userListings, setUserListings] = useState([]);
 const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -99,6 +100,24 @@ const dispatch = useDispatch();
     }
   };    // after backend api craete sign out redux'
 
+
+  const handleShowListings = async () => {
+    try {
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
+
   console.log( formData);
     return (
    
@@ -153,11 +172,7 @@ const dispatch = useDispatch();
         >
           Create Listing
         </Link>
-        <button 
-        // onClick={handleShowListings}
-         className='text-green-700 w-full'>
-        Show Listings
-      </button>
+       
 
         </form>
         <div className='flex justify-between mt-5'>
@@ -178,6 +193,54 @@ const dispatch = useDispatch();
       <p className='text-green-700 mt-5'>
         {updateSuccess ? 'User is updated successfully!' : ''}
       </p>
+
+      <button 
+        onClick={handleShowListings}
+         className='text-green-700 w-full'>
+        Show Listings
+      </button>
+      <p className='text-red-700 mt-5'>
+        {showListingsError ? 'Error showing listings' : ''}
+      </p>
+      {userListings && userListings.length > 0 && (
+        <div className='flex flex-col gap-4'>
+          <h1 className='text-center mt-7 text-2xl font-semibold'>
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className='border rounded-lg p-3 flex justify-between items-center gap-4'
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt='listing cover'
+                  className='h-16 w-16 object-contain'
+                />
+              </Link>
+              <Link
+                className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+
+              <div className='flex flex-col item-center'>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className='text-red-700 uppercase'
+                >
+                  Delete
+                </button>
+                <Link to={`/update-listing/${listing._id}`}>
+                  <button className='text-green-700 uppercase'>Edit</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+           )}
     </div>
   )
 }
