a1ad6b832db767f2c5659447b055d5f99994fbbb
Aiyasha99 <aiyashapinjari99@gmail.com>
2024-07-04 11:41:32 +0530

    home page , about page and configuration to deploy

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
 import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ mongoose.connect(process.env.MONGO).then(() =>{
 console.log(err);
 
 });
+
+const __dirname=path.resolve();
 
 const app=express();
 
@@ -29,6 +32,13 @@ console.log('server is running on port 5000 @');
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next)=>{
     const statusCode= err.statusCode || 500;
